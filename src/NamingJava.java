785d64c363ccedfd5776d7b31391bed756299131
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-28 10:32:22 -0700

    12/39 Java namig convention

---
+++ src/NamingJava.java
@@ -0,0 +1,19 @@
+public class NamingJava {
+    public static void main(String[] args){
+        int celphone = 33377722;
+        int celPhone = 55558777;
+
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXM";
+        String background_color = "BLUE";
+
+        final int POSITION = -5;
+        final int MAX_WIDTH = 999;
+        final int MIN_WIDTH = 1;
+    }
+}
