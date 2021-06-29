6dab5ebecc02f5ed8e920bcf49c4dc277e883909
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-28 20:27:46 -0700

    37/39 Arrays indices in Java

--- 8 Java version/src/Arrays.java
+++ 8 Java version/src/Arrays.java
@@ -13,5 +13,32 @@ public class Arrays {
         **/
         int [][][] numbers = new int[2][2][2];
         int [][][][] numbers4 = new int[2][2][2][2];
+
+        androidVersions[0] = "Apple pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+        cities[0][0]="México";
+        cities[0][1]="CDMX";
+        cities[1][0]="México";
+        cities[1][1]="Guadalajara";
+        cities[2][0]="Colombia";
+        cities[2][1]="Bogotá";
+        cities[3][0]="Colombia";
+        cities[3][1]="Medellín";
+
+        for(int i=0; i<4; i++)
+        {
+            System.out.println("Country: " + cities[i][0]+ " City:" + cities[i][1]);
+        }
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
     }
 }
