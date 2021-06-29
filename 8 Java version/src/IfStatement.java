1956580836e1a7881b2dfb0d035a776eea98e8b3
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-28 17:32:29 -0700

    24/39 Variable scope and Else Statement

--- 8 Java version/src/IfStatement.java
+++ 8 Java version/src/IfStatement.java
@@ -1,6 +1,6 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSended =3;
 
         if(isBluetoothEnabled == true)
@@ -8,6 +8,12 @@ public class IfStatement {
             //Send file
             fileSended++;
             System.out.println("File was send");
+            int i =0;
+            i++;
+        }
+        else
+        {
+            System.out.println("Turn on the Bluetooth");
         }
     }
 }
