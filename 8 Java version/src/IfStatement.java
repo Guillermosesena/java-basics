2a4d5c52c1926573ecfac8e91bf8a7f2833ee9e8
Guillermo Sesena <g.sesena.pascacio@accenture.com>
2021-06-28 17:12:25 -0700

    22/39 If statement

---
+++ 8 Java version/src/IfStatement.java
@@ -0,0 +1,13 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled = true;
+        int fileSended =3;
+
+        if(isBluetoothEnabled == true)
+        {
+            //Send file
+            fileSended++;
+            System.out.println("File was send");
+        }
+    }
+}
